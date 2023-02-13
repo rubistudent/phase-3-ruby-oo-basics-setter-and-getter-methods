465b01f946058c61f6ab343a920c4fc6297bd681
www.rubiluqman7@gmail.com <www.rubiluqman7@gmail.com>
2023-02-13 15:11:48 +0300

    completed

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,17 @@
-class Dog
+require 'pry'
 
+class Dog
+    #setter for name
+    def name = (name)
+        @name = name
+    end
+    #getter method
+    def name
+        @name
+    end
+
+    # 
+    def  breed =(breed)
+        @breed = breed
+    end
 end
